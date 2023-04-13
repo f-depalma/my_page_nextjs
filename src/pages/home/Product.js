90d891aefcc55dd1640532a8021b227dfdeb1ed3
f.depalma <325587@via.dk>
2023-04-13 20:59:36 +0200

    fix responsivenes

--- src/pages/home/Product.js
+++ src/pages/home/Product.js
@@ -1,7 +1,7 @@
 export default function Product({ icon, name, company, description }) {
     return (
         <div className="product w-60 p-6 rounded bg-white shadow-lg">
-            <img src={icon} className="pb-4 w-32 h-32 object-contain m-auto" />
+            <img src={icon} className="pb-4 w-32 h-16 lg:h-32 object-contain m-auto" />
             <h4 className="text-center text-2xl font-bold pb-2 text-zinc-600">{name}</h4>
             <h4 className="text-center font-bold pb-2 text-yellow-600 ">{company}</h4>
             <p className="text-center text-zinc-700">{description}</p>
--- src/pages/home/Skill.js
+++ src/pages/home/Skill.js
@@ -1,9 +1,9 @@
 export default function Skill({ icon, name, description }) {
     return (
-        <div className="w-80 m-auto">
-            <img src={icon} className="w-28 pb-4 object-contain m-auto" />
-            <h4 className="text-center text-xl font-bold pb-2 text-zinc-700">{name}</h4>
-            <p className="text-center">{description}</p>
+        <div className="w-60 lg:w-80 m-auto">
+            <img src={icon} className="w-auto h-20 lg:h-24 pb-4 object-contain m-auto" />
+            <h4 className="text-center text-lg lg:text-xl font-bold pb-2 text-zinc-700">{name}</h4>
+            <p className="text-center text-sm lg:text-base">{description}</p>
         </div>
     )
 }
