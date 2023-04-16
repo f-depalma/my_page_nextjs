17ba2fcb9ba43c6836cf55be1acc1590cefa398f
f.depalma <325587@via.dk>
2023-04-16 04:52:52 +0200

    color inverted buttons my project

--- src/pages/my-projects/Project.js
+++ src/pages/my-projects/Project.js
@@ -10,12 +10,12 @@ export default function Project({ title, thumbnail, description, linkWebsite, li
                 <h1 className='font-bold lg:text-md xl:whitespace-nowrap text-ellipsis overflow-hidden'>{title}</h1>
                 <p className='mt-2 text-sm'>{description}</p>
                 <div className='flex flex-row justify-around gap-4 mt-4'>
-                    <div className='w-32 rounded flex flex-col justify-center bg-yellow-600 text-center h-10 hover:bg-zinc-600'>
+                <div className='w-32 text-yellow-600 rounded flex flex-col justify-center bg-zinc-900 text-center h-10 hover:bg-zinc-600'>                    
                         <a href={linkGithub} target="_blank">
                             GITHUB
                         </a>
                     </div>
-                    <div className='w-32 text-yellow-600 rounded flex flex-col justify-center bg-zinc-900 text-center h-10 hover:bg-zinc-600'>
+                    <div className='w-32 rounded flex flex-col justify-center bg-yellow-600 text-center h-10 hover:bg-zinc-600'>
                         <a href={linkWebsite} target="_blank">
                             WEBSITE
                         </a>
